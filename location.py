0b0a239e8312dec6975f7a43a36c745b9f83652e
WTR-GIT <saulius.anusas@gmail.com>
2024-03-14 22:04:29 +0200

    Updated so no we get city and coordinates

--- location.py
+++ location.py
@@ -3,5 +3,9 @@ from requests import get
 response = get('https://httpbin.org/ip')
 r = response.json()
 
-loc = get(f"https://ipapi.co/{r['origin']}/city/")
-location = loc.json()
+loc = get(f"https://ipapi.co/{r['origin']}/json/")
+print (loc.json())
+location_data = loc.json()
+city = location_data["city"]
+latitude = location_data["latitude"]
+longitude = location_data["longitude"]
