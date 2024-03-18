79e0b6128dc9b53206e3aea90613e092745a8431
Mindaugas Minelga <minelga.m@gmail.com>
2024-03-18 18:19:26 +0200

    add Blueprint structure

---
+++ app/main/routes.py
@@ -0,0 +1,16 @@
+from flask import render_template
+from app.main import bp
+from requests import get
+
+
+@bp.route("/")
+def index():
+    response = get("https://httpbin.org/ip")
+    r = response.json()
+    ip = r["origin"]
+    loc = get(f"https://ipapi.co/{r['origin']}/json/")
+    city_json = loc.json()
+    city = city_json["city"]
+    print(city)
+
+    return render_template("index.html", user_location=city, user_ip=ip)
